755e51555737ceb01ec272b889d8ca8bcc2295fa
tunapro <tuna.gul@nfrproducts.com>
2024-08-10 21:26:41 +0300

    update plugin

--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,3 @@
-# name: discourse-step-iframe
-# about: Automatically embeds an iframe for STEP files
-# version: 0.1
-# authors: Tuna Gül
-# url: https://github.com/tunapro1234/discourse-step-iframe
-
 enabled_site_setting :discourse_step_iframe_enabled
 
 after_initialize do
@@ -13,9 +7,13 @@ after_initialize do
         file_url = upload.url
         iframe_code = "<iframe src='https://sharecad.org/cadframe/load?url=#{file_url}&lang=en&user=0' width='100%' height='600' frameborder='0'></iframe>"
 
-        # Insert iframe after the upload link in the post
-        doc.css("a[href='#{upload.url}']").after(iframe_code)
-        post.revise(Discourse.system_user, raw: doc.to_html)
+        # Insert iframe after the upload link in the post content
+        doc.css("a[href='#{upload.url}']").each do |link|
+          link.after(iframe_code)
+        end
+
+        # Update the post content
+        post.update(cooked: doc.to_html)
       end
     end
   end
